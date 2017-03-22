1b1e1f77901ac89588fbff61f750c8c2f9bbb318
finde <finde@DESKTOP-TRRO64V>
2017-03-22 14:20:35 +0100

    Création DAO Visiteur + Labo + Secteur + corrections

--- src/controleur/CtrlVisiteur.java
+++ src/controleur/CtrlVisiteur.java
@@ -1,59 +1,130 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controleur;
 
-
-
-import vue.F_VISITEUR;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import modele.metier.Visiteur;
+import vue.F_VISITEUR;
+import java.util.List;
+import modele.dao.DaoVisiteur;
+import modele.metier.Secteur;
 
+public class CtrlVisiteur implements ActionListener {
 
-/**
- *
- * @author btssio
- */
-public class CtrlVisiteur implements ActionListener{
-    private F_VISITEUR vue; // LA VUE
-    CtrlPrincipal ctrlPrincipal;
-    
-   
+    private F_VISITEUR vue;
+    private Visiteur modele;
+  
+    private List<Visiteur> lesVisiteurs;
+    private modele.metier.Visiteur unVisiteur;
+    private String login;
 
-
-
-    public CtrlVisiteur(F_VISITEUR vue, CtrlPrincipal leControleurPrincipal) {
+    public CtrlVisiteur(F_VISITEUR vue) {
+        System.out.println ("Controller()");
         this.vue = vue;
-        this.ctrlPrincipal = leControleurPrincipal;
-        vue.getjButtonPrecedent1().addActionListener(this);
-        vue.getjButtonSuivant1().addActionListener(this);
-        vue.getjButtonFermer().addActionListener(this);
+        afficherLesVisiteurs();
+        
+        getVue().getjButtonOk().addActionListener(this);
+        getVue().getjButtonSuivant1().addActionListener(this);
+        getVue().getjButtonPrecedent1().addActionListener(this);
+        getVue().getjButtonFermer().addActionListener(this);
+
     }
     
+    public void addModel(Visiteur m){
+        System.out.println("Controller: adding model");
+	this.modele = m;
+    } //addModel()
+
+    public void addView(F_VISITEUR v){
+	System.out.println("Controller: adding view");
+	this.vue = v;
+    } //addView()
+
+    public final void afficherLesVisiteurs() {
+        try {
+            lesVisiteurs = DaoVisiteur.selectAll();
+            for (Visiteur visiteur : lesVisiteurs) {
+                vue.getjComboBoxChercher().addItem(visiteur);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            ex.printStackTrace();
+        }
+
+    }
+
+    @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
-        if (source == vue.getjButtonFermer()) {
-            ctrlPrincipal.afficherMenu(vue);
-        }
-        if (source == vue.getjButtonPrecedent1()) {
-            ctrlPrincipal.afficherRapportVisite(vue);
-        }
-        if (source == vue.getjButtonSuivant1()) {
-            ctrlPrincipal.afficherPraticien(vue);
-        }
-    }
+        System.out.println("test");
+        if (source == vue.getjButtonOk()) {
+            unVisiteur = (Visiteur) vue.getjComboBoxChercher().getSelectedItem();
+            vue.getjTextFieldNom().setText(unVisiteur.getNom());
+            vue.getjTextFieldPrenom().setText(unVisiteur.getPrenom());
+            vue.getjTextFieldAdresse().setText(unVisiteur.getAdresse());
+            vue.getjTextFieldVille().setText(unVisiteur.getVille());
+            Secteur secteur = unVisiteur.getSecteur();
+            if (secteur == null) {
+              //  vue.getJTextFieldSecteur().setText("");
+            } else {
+              //  vue.getjTextFieldSecteur().setText(unVisiteur.getSecteur().getSec_libelle());
+            }
 
-    public F_VISITEUR getVue() {
-        return vue;
-    }
+           // vue.getjTextFieldLabo().setText(unVisiteur.getLabo().getLab_nom());
 
-    public void setVue(F_VISITEUR vue) {
-        this.vue = vue;
-    }
-   
-    
+        }
+
+      /*  if (source == vue.getjButtonSuivant()) {
+            int i = vue.getjComboBoxListeVisiteurs().getSelectedIndex();
+            int z = i + 1;
+            if (z < vue.getjComboBoxListeVisiteurs().getItemCount()) {
+                unVisiteur = (MetierVisiteur) vue.getjComboBoxListeVisiteurs().getItemAt(z);
+                vue.getjComboBoxListeVisiteurs().setSelectedIndex(z);
+                vue.getjTextFieldNom().setText(unVisiteur.getNom());
+                vue.getjTextFieldPrenom().setText(unVisiteur.getPrenom());
+                vue.getjTextFieldAdresse().setText(unVisiteur.getAdresse());
+                vue.getjTextFieldVille().setText(unVisiteur.getVille());
+                Secteur secteur = unVisiteur.getSecteur();
+                if (secteur == null) {
+                    vue.getjTextFieldSecteur().setText("");
+                } else {
+                    vue.getjTextFieldSecteur().setText(unVisiteur.getSecteur().getSec_libelle());
+                }
+                vue.getjTextFieldLabo().setText(unVisiteur.getLabo().getLab_nom());
+
+            }
+
+        }
+
+      /*  if (source == vue.getjButtonPrecedent()) {
+            int i = vue.getjComboBoxListeVisiteurs().getSelectedIndex();
+            int z = i - 1;
+            if (z >= 0) {
+                unVisiteur = (MetierVisiteur) vue.getjComboBoxListeVisiteurs().getItemAt(z);
+                vue.getjComboBoxListeVisiteurs().setSelectedIndex(z);
+                vue.getjTextFieldNom().setText(unVisiteur.getNom());
+                vue.getjTextFieldPrenom().setText(unVisiteur.getPrenom());
+                vue.getjTextFieldAdresse().setText(unVisiteur.getAdresse());
+                vue.getjTextFieldVille().setText(unVisiteur.getVille());
+
+                Secteur secteur = unVisiteur.getSecteur();
+                if (secteur == null) {
+                    vue.getjTextFieldSecteur().setText("");
+                } else {
+                    vue.getjTextFieldSecteur().setText(unVisiteur.getSecteur().getSec_libelle());
+                }
+
+                vue.getjTextFieldLabo().setText(unVisiteur.getLabo().getLab_nom());
+
+            }*/
+
+        }
+
+     /*   if (source == vue.getjButtonFermer()) {
+            vue.dispose();
+            vueMenu = new VueMenu();
+            CtrlMenu controllers = new CtrlMenu(vueMenu, );
+            vueMenu.setVisible(true);
+        }*/
 }
-
-
